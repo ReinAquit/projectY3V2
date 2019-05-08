0fe3f4a9f96f24affe9e0de116de8f2e3acb4fa8
ReinAquit <aquit4life@gmail.com>
2019-05-08 10:34:39 +0200

    initial commit of Y3V4...

---
+++ OpdrachtY3V3/OpdrachtY3V3/src/main.c
@@ -0,0 +1,40 @@
+//--------------------------------------------------------------
+// File     : main.c
+// Datum    : 30.03.2016
+// Version  : 1.0
+// Autor    : UB
+// mods by	: J.F. van der Bent
+// CPU      : STM32F4
+// IDE      : CooCox CoIDE 1.7.x
+// Module   : CMSIS_BOOT, M4_CMSIS_CORE
+// Function : VGA_core DMA LIB 320x240, 8bit color
+//--------------------------------------------------------------
+
+#include "main.h"
+#include "stm32_ub_vga_screen.h"
+#include <math.h>
+
+int main(void)
+{
+	int i;
+
+	//  uint32_t n;
+
+	SystemInit(); // System speed to 168MHz
+
+	UB_VGA_Screen_Init(); // Init VGA-Screen
+
+    UB_VGA_FillScreen(VGA_COL_GREEN);
+    UB_VGA_SetPixel(10,10,10);
+
+    char test[] = "test string";
+
+    puts(test);
+
+  while(1)
+  {
+	  i++;
+	  i--;
+  }
+}
+
