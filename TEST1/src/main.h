ee26ed1f79bffe4c43f99bf2a2bf51a7780803d4
ReinAquit <aquit4life@gmail.com>
2019-05-29 12:41:01 +0200

    working version

---
+++ TEST1/src/main.h
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------
+// File     : main.h
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+#ifndef __STM32F4_UB_MAIN_H
+#define __STM32F4_UB_MAIN_H
+
+//Defines
+
+//--------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------
+#include <stm32f4xx1.h>
+#include <stm32f4xx_usart.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_gpio.h>
+#include <math.h>
+
+#include "ui.h"
+#include "uart.h"
+//#include "api.h"
+
+#include "stm32_ub_vga_screen.h"
+#include "stm32f4xx_usart.h"
+
+//--------------------------------------------------------------
+#endif // __STM32F4_UB_MAIN_H
